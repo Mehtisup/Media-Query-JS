faa8b23af6da4071cd69c7206fe1e66d18c1c34b
Mahdi Bozorgi <102772767+Mehtisup@users.noreply.github.com>
2023-03-09 05:31:45 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-If the viewport is less than 720px or equal to 720px wide, the background color of the web page will be yellow. In this repository I'm going to demonstrate how to make it using JavaScript for the website.
+<script>
+function viewFunction(z) {
+    if (z.matches) {
+        document.getElementById("body").style.backgroundColor = "yellow";
+    } else {
+        document.getElementById("body").style.backgroundColor = "white";
+    }
+}
+
+var z = window.matchMedia("(max-width:720px)");
+
+viewFunction(z);
+
+z.addListener(viewFunction);
+</script>
